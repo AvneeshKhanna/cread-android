ca8d70f17d8de4335e2ae0ddbe27b5a5b4825f5c
gaurav_24 <itsgaurav@live.in>
2017-10-06 16:30:42 +0530

    Added gradle in git Created Application class Added dependency in build file

---
+++ app/src/main/java/com/thetestament/cread/CreadApp.java
@@ -0,0 +1,10 @@
+package com.thetestament.cread;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by gaurav on 06/10/17.
+ */
+
+public class CreadApp extends MultiDexApplication {
+}
