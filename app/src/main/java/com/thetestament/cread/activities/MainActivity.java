a9d7eda9b2a78b8f65e9186a0f7ab4854c40c67b
gaurav_24 <itsgaurav@live.in>
2017-11-15 21:29:40 +0530

    Added product tour system and minor modifications.

--- app/src/main/java/com/thetestament/cread/activities/MainActivity.java
+++ app/src/main/java/com/thetestament/cread/activities/MainActivity.java
@@ -3,7 +3,6 @@ package com.thetestament.cread.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
