0ee505342a57bf8b1d69c073c811ee9bfcc1ed84
gaurav_24 <itsgaurav@live.in>
2018-03-01 20:01:53 +0530

    Remove "Cread" while sharing content from cread itself.

--- app/src/main/java/com/thetestament/cread/helpers/ShareHelper.java
+++ app/src/main/java/com/thetestament/cread/helpers/ShareHelper.java
@@ -5,10 +5,16 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Parcelable;
 
 import com.thetestament.cread.R;
 import com.thetestament.cread.models.FeedModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.thetestament.cread.helpers.ImageHelper.getLocalBitmapUri;
 
@@ -33,11 +39,39 @@ public class ShareHelper {
         //Show toast
         ViewHelper.getToast(context, "Caption copied! Paste before sharing");
 
+        //Obtain uri of image to be shared
+        Uri uri = getLocalBitmapUri(bitmap, context);
+
+        List<Intent> targets = new ArrayList<>();
+        Intent template = new Intent(Intent.ACTION_SEND);
+        template.setType("image/*");
+
+        List<ResolveInfo> candidates = context.getPackageManager().
+                queryIntentActivities(template, 0);
+
+        //Exclude cread app
+        for (ResolveInfo resolveInfo : candidates) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            if (!packageName.equals("com.thetestament.cread")) {
+                Intent target = new Intent(Intent.ACTION_SEND);
+                target.setType("image/*");
+                target.putExtra(Intent.EXTRA_STREAM, uri);
+                target.setPackage(packageName);
+                targets.add(target);
+            }
+        }
+
+        Intent chooser = Intent.createChooser(targets.remove(0), "Share");
+        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS,
+                targets.toArray(new Parcelable[targets.size()]));
+        context.startActivity(chooser);
+
+
         //Create intent chooser
-        Intent intent = new Intent();
+        /*Intent intent = new Intent();
         intent.setAction(Intent.ACTION_SEND);
-        intent.setType("image/*");
+        intent.setType("image*//* ");
         intent.putExtra(Intent.EXTRA_STREAM, getLocalBitmapUri(bitmap, context));
-        context.startActivity(Intent.createChooser(intent, "Share"));
+        context.startActivity(Intent.createChooser(intent, "Share"));*/
     }
 }
